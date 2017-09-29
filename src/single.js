4b7cbffb5830d1fd524a9a5b471ddeb0f639afbc
xmhscratch <xmhscratch@gmail.com>
2017-09-29 14:21:30 +0700

    Update single.js

--- src/single.js
+++ src/single.js
@@ -1,8 +1,31 @@
 const Db = require('./db')
+const DbTable = require('./tenancy/db-table')
 
 class SingleTenancy {
-    constructor(tenantName) {
-        return new Db().connect(tenantName)
+
+    get tables() {
+        return this._tables || {}
+    }
+
+    constructor() {
+        this._initialized = false
+        return this
+    }
+
+    initialize(tableName) {
+        this.tableName = tableName
+
+        return new DbTable(tableName)
+            .ensure()
+            .then((dbTable) => {
+                return dbTable.getDb()
+            })
+            .then((db) => {
+                this._tables = db.Tables
+                this._initialized = true
+                return this
+            })
+            .thenReturn(this)
     }
 }
 
